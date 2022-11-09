188dbd908e2e3e0ad131732f402b814749d9b9cb
sayedraminh <raminhashemi2016@gmail.com>
2022-11-09 01:13:10 +0000

    activity 1

---
+++ unit_07/index.js
@@ -0,0 +1,49 @@
+
+
+let row = document.getElementById("r1");
+let coll1 = document.getElementById("col1");
+let coll2 = document.getElementById("col2");
+let coll3 = document.getElementById("col3");
+let coll4 = document.getElementById("col4");
+let coll5 = document.getElementById("col5");
+let coll6 = document.getElementById("col6");
+let coll7 = document.getElementById("col7");
+let coll8 = document.getElementById("col8");
+
+function rowOrignalColor(){
+    row.style.backgroundColor = 'black';
+}
+
+function col1 () {
+    
+    row.style.backgroundColor = 'red';
+}
+
+function col2 () {
+    
+    row.style.backgroundColor = 'rgb(2, 145, 52)';
+}
+function col3 () {
+    
+    row.style.backgroundColor = 'rgb(0, 30, 255)';
+}
+function col4 () {
+    
+    row.style.backgroundColor = 'rgb(255, 0, 191)';
+}
+function col5 () {
+    
+    row.style.backgroundColor = 'rgba(163, 191, 48, 0.154)';
+}
+function col6 () {
+    
+    row.style.backgroundColor = 'rgb(255, 0, 98)';
+}
+function col7 () {
+    
+    row.style.backgroundColor = 'rgba(0, 187, 255, 0.919)';
+}
+function col8 () {
+    
+    row.style.backgroundColor = 'rgba(0, 51, 155)';
+}
